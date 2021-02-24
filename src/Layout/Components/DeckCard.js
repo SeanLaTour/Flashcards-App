f612a5aa0c50c066900b4d29eaa1bf5e60cd8f4c
Sean La Tourette <latourettes@live.com>
2021-02-23 18:01:48 -0800

    update

--- src/Layout/Components/DeckCard.js
+++ src/Layout/Components/DeckCard.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Trash, Eye, Book } from "react-bootstrap-icons";
-import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { listCards } from "../../utils/api/index.js";
 
@@ -16,57 +14,55 @@ function DeckCard({ deck, index, handleDeleteDeck }) {
   }, []);
 
   return (
-    <Card id={deck.id} className="mt-3 mb-3" key={index}>
-      <Card.Body>
-        <Container>
-          <Row>
-            <Col>
-              <Card.Title>{deck.name}</Card.Title>
-            </Col>
-            <Col>
+    <div id={deck.id} className="mt-3 mb-3" key={index}>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h2>{deck.name}</h2>
+            </div>
+            <div className="col">
               <p className="text-right">{count} cards</p>
-            </Col>
-          </Row>
-        </Container>
-        <Row>
-          <Col>
-            <Card.Text className="m-3">{deck.description}</Card.Text>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <p className="m-3">{deck.description}</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
             <Link to={`/decks/${deck.id}`}>
-              <Button variant="secondary" className="m-1">
-                <Eye />
+              <button variant="secondary" className="m-1">
+              <span class="oi oi-eye"></span>
                 View
-              </Button>
+              </button>
             </Link>
             <Link to={`/decks/${deck.id}/study`}>
-              <Button onClick={() => listCards(deck.id)}>
-                <Book />
+              <button onClick={() => listCards(deck.id)}>
+              <span class="oi oi-book"></span>
                 Study
-              </Button>
+              </button>
             </Link>
-          </Col>
-          <Col className="col-sm-1 col-lg-1 mr-lg-1">
-            <Button
+          </div>
+          <div className="col-sm-1 col-lg-1 mr-lg-1">
+            <button
               variant="danger"
               className="m-1"
               onClick={() => {
                 if (
                   window.confirm(
-                    "You sure you want to delete that dawg? You won't get it back"
+                    "Are you sure you want to delete? You won't be able to get it back"
                   )
                 )
                   handleDeleteDeck(deck.id);
               }}
             >
-              <Trash />
-            </Button>
-          </Col>
-        </Row>
-      </Card.Body>
-    </Card>
+              <span class="oi oi-trash"></span>
+            </button>
+          </div>
+        </div>
+    </div>
   );
 }
 
