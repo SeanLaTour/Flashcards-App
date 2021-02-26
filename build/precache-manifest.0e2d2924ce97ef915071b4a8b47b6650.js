d0cdc0f3b6370a219e24bf74a93e52e654e22b01
Sean La Tourette <latourettes@live.com>
2021-02-25 18:20:42 -0800

    for personal

---
+++ build/precache-manifest.0e2d2924ce97ef915071b4a8b47b6650.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d71dd3a58c3b7ec07ed21f2ffc16727",
+    "url": "/Flashcards-App/index.html"
+  },
+  {
+    "revision": "3824dc6613f9b76a13a7",
+    "url": "/Flashcards-App/static/js/2.8bb908e7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Flashcards-App/static/js/2.8bb908e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993f798a009a2121372a",
+    "url": "/Flashcards-App/static/js/main.98cea2a1.chunk.js"
+  },
+  {
+    "revision": "3ba97c8560017675ef64",
+    "url": "/Flashcards-App/static/js/runtime-main.d4e1dd0f.js"
+  }
+]);
